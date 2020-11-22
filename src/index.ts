111bc2ed1f98195ad1423f31537041c6f6236c7c
arifali123 <51419655+arifali123@users.noreply.github.com>
2020-11-21 23:06:42 -0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,36 @@ const underline = "__";
 const strikethrough = "~~";
 const italic = "*";
 const bold = "**";
-function reverseString(str: string) {
-  return str.split("").reverse().join("");
-}
 const GenMDOptionsDefault: GenMDOptions = {
   underline: false,
   strikethrough: false,
   italic: false,
   bold: false,
 };
-interface GenMDOptions {
-  underline?: boolean;
-  strikethrough?: boolean;
-  italic?: boolean;
-  bold?: boolean;
-}
 const edits: { [key: string]: string } = {
   underline,
   strikethrough,
   italic,
   bold,
 };
+function reverseString(str: string) {
+  return str.split("").reverse().join("");
+}
+export interface FDobject {
+  name: string;
+  value: string;
+}
+export interface FDoptions {
+  separator?: string;
+  nameEdits?: GenMDOptions;
+  valueEdits?: GenMDOptions;
+}
+export interface GenMDOptions {
+  underline?: boolean;
+  strikethrough?: boolean;
+  italic?: boolean;
+  bold?: boolean;
+}
 function GenMD(
   text: string,
   options: GenMDOptions = GenMDOptionsDefault
@@ -35,15 +44,7 @@ function GenMD(
     .join("");
   return sep + text + reverseString(sep);
 }
-export interface FDobject {
-  name: string;
-  value: string;
-}
-export interface FDoptions {
-  separator?: string;
-  nameEdits?: GenMDOptions;
-  valueEdits?: GenMDOptions;
-}
+
 export function FD(
   values: FDobject[],
   options: FDoptions = {
